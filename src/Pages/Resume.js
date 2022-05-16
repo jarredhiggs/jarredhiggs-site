cbf372356dadf5b64c9ea3e43c8cbc0d77a89441
Jarred Higgs <jarred.higgs@gmail.com>
2022-05-16 01:11:50 -0400

    Updated Resume

--- src/Pages/Resume.js
+++ src/Pages/Resume.js
@@ -7,15 +7,26 @@ function Resume() {
             <h1 style={{ "text-align": "center", "margin-bottom": "15px" }}>Resume</h1>
             <div className="resume-row">
                 <div className="resume-column">
-                    <h2>Professional Experience</h2>
+                    <h2>Employment Experience</h2>
                     <ResumeWorkItem
-                        name={"Fannie Mae"}
+                        name="Walmart (Motion Recruitment)"
+                        title="Backend Engineer"
+                        date="January 2022 - July 2022 (Planned)"
+                        description={[
+                            "Developed high traffic APIs (10,000+ TPS) for global payment processing",
+                            "Rearchitected orchestration framework to enable high throughput with minimal latency and minimal resource consumption (and to substantially improve code readability and maintainability)",
+                            "Cooperated with teams across numerous companies to integrate multiple external payment processors into our platform"
+                        ]}
+                    />
+                    <ResumeWorkItem
+                        name="Fannie Mae"
                         title="Full Stack Developer, Cloud Architect"
                         date="February 2021 - July 2021"
                         description={[
-                            "Migrated application systems to AWS architecture",
-                            "Automated integration and deployment",
-                            "Implemented real-time monitoring of high-traffic applications"
+                            "Developed functionality for Fannie Mae's automated loan underwriting system",
+                            "Migrated applications from on-premesis servers to cloud infrastructure (AWS)",
+                            "Integrating monitoring/logging systems for auditing and incident resolution",
+                            "Rearchitected cloud infrastructure for various applications to adhere to evolving business standards and requirements"
                         ]}
                     />
                     <ResumeWorkItem
@@ -23,8 +34,10 @@ function Resume() {
                         title="Full Stack Developer"
                         date="August 2020 - December 2020"
                         description={[
-                            "Developed and maintained Spring API microsystems",
-                            "Overhauled CI/CD process via Jenkins, OpenShift Container Platform"
+                            "Software Developer for The Department of Homeland Security's Citizenship and Immigration Services",
+                            "Developed and maintained APIs for automation of immigration application processes",
+                            "Overhauled CI/CD process using Jenkins and OpenShift Container Platform",
+                            "Contract ended early due to COVID-19 rebudgeting"
                         ]}
                     />
                     <ResumeWorkItem
@@ -34,9 +47,10 @@ function Resume() {
                         description={[
                             "Swift and Obj-C Developer for CreditWise iOS",
                             "Secondary role as Angular developer for CW Web",
-                            "Improved CI/CD automation through Jenkins and Chef",
-                            "Enhanced the team's unit testing and acceptance testing practices, substantially reducing the occurrence of bugs in both QA and in Production",
-                            "Led the team on establishing accessibility and content localization requirements across the application"
+                            "Led the team on establishing accessibility and content localization standards throughout the application",
+                            "Cooperated with Product Owners and UX Designers to create solutions that match product expectations without compromising on proper development practices",
+                            "Improved the team's unit testing and acceptance testing practices, substantially reducing the incidence of bugs that would otherwise have been missed until QA testing or production deployment",
+                            "Improved CI/CD automation using Jenkins and Chef",
                         ]}
                     />
                     <ResumeWorkItem
@@ -44,13 +58,11 @@ function Resume() {
                         title="API Developer, Cloud Architect"
                         date="February 2016 - February 2017"
                         description={[
-                            "Developed high-traffic microservices using Spring",
-                            "Replaced on-premesis batch processes with real-time processes hosted on AWS architecture",
-                            "Designed and implemented Postgres schema",
-                            "Designed and implemented deployment automation strategies to optimize scalability and resiliency against operational costs",
-                            "Created acceptance testing utility for product owners to automatically generate tests from Excel data",
-                            "Cooperated with teams across the enterprise to define appropriate API specifications and service level agreements",
-                            "Worked with product owners to define and implement monitoring and logging requrements that properly meet business requirements"
+                            "Developed Spring Boot APIs",
+                            "Developed an Orchestration API to integrate our APIs with an upstream decision engine",
+                            "Replaced on-premesis batch processes (6 month intervals) with real-time processes hosted on AWS architecture",
+                            "Designed and implemented deployment automation strategies to optimize scalability and resiliency while minimizing operational costs",
+                            "Created acceptance testing utility for product owners to automatically generate tests from Excel data, eliminating involvement of developers in testing"
                         ]}
                     />
                 </div>
@@ -62,34 +74,58 @@ function Resume() {
                         B.A. Computer Science <br />
                         B.A. Biology <br />
                     </div> <br />
+                    <h2>Certifications</h2>
+                    <h3>AWS (Associate)</h3>
+                    <ul>
+                        <li>Developer</li>
+                        <li>Solutions Architect</li>
+                    </ul>
                     <h2>Skills</h2>
                     <h3>iOS</h3>
                     <ul>
-                        <li>Swift</li>
-                        <li>Objective-C</li>
-                        <li>RxSwift</li>
-                        <li>UI Automation Testing</li>
+                        <li>Swift App Dev</li>
+                        <li>Obj-C App Dev</li>
+                        <li>Reactive<ul>
+                            <li>RxSwift</li>
+                        </ul></li>
+                        <li>UI Testing<ul>
+                            <li>EarlGrey</li>
+                        </ul></li>
                     </ul>
-                    <h3>Full Stack Development</h3>
+                    <h3>Full Stack</h3>
                     <ul>
-                        <li>Spring</li>
-                        <li>CI/CD Automation (Chef, Jenkins, Openshift)</li>
-                        <li>Build Automation (Maven, Gradle)</li>
-                        <li>Cucumber/Karate</li>
-                        <li>PostgreSQL</li>
-                        <li>MongoDB</li>
-                        <li>Chef</li>
-                        <li>Python</li>
-                        <li>Application Monitoring (Splunk, CloudWatch, CloudTrail)</li>
+                        <li>Spring API Dev<ul>
+                            <li>MVC</li>
+                            <li>WebFlux</li>
+                        </ul></li>
+                        <li>CI/CD Automation <ul>
+                            <li>Chef</li>
+                            <li>Jenkins</li>
+                            <li>OpenShift</li>
+                            <li>Kubernetes</li>
+                        </ul></li>
+                        <li>Acceptance Testing <ul>
+                            <li>Gherkin</li>
+                            <li>Cucumber</li>
+                        </ul></li>
+                        <li>Database<ul>
+                            <li>PostgreSQL</li>
+                            <li>MongoDB</li>
+                            <li>Google Spanner</li>
+                            <li>DynamoDB</li>
+                        </ul></li>
+                        <li>Scripting<ul>
+                            <li>Python</li>
+                            <li>Bash</li>
+                        </ul></li>
+                        <li>Monitoring<ul>
+                            <li>Splunk</li>
+                            <li>CloudWatch</li>
+                        </ul></li>
                     </ul>
-                    <h3>Web App Development</h3>
+                    <h3>Web App</h3>
                     <ul>
                         <li>React</li>
-                    </ul>
-                    <h3>AWS</h3>
-                    <ul>
-                        <li>Associate Solutions Architect Certification</li>
-                        <li>Associate Developer Certification</li>
                     </ul>
                 </div>
             </div>
