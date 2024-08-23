6e789c404854017f2046ed7c865415d9a7ae4034
Jarred H <jarred.higgs@gmail.com>
2024-08-22 20:22:49 -0400

    Update Footer.js

--- src/MainComponents/Footer.js
+++ src/MainComponents/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 function Footer() {
     return (
         <div className="Footer">
-            <div id="email">[MyDomainName] @ gmail</div>
+            <div id="email">[MyDomainName] at gmail</div>
             © 2024 Jarred Higgs
         </div>
     )
